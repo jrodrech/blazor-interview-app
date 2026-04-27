e219e08fbc86e53c5d2293be58c524392cfc675b
Joel Rodriguez <jrodrech@gmail.com>
2026-04-26 22:17:30 -0400

    Initial commit

---
+++ BlazorInterviewApp.Api/Controllers/Base.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorInterviewApp.Api.Controllers
+{
+    public class Base : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("tickets")]
+        public List<string> GetTickets()
+        {
+            return new List<string> { "Ticket A", "Ticket B", "Ticket C" };
+        }
+    }
+}
---
+++ BlazorInterviewApp.Api/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorUI",
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:5001")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors("AllowBlazorUI");
+
+app.MapControllers();
+
+app.Run();
